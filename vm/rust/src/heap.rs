3381ff3714c5c57fbb4d27fc609466df3d2e8e11
Yota Toyama <raviqqe@gmail.com>
2025-09-17 20:37:11 -0700

    Heap (#11)

---
+++ vm/rust/src/heap.rs
@@ -0,0 +1,9 @@
+/// A heap memory.
+pub trait Heap<T>: AsRef<[T]> + AsMut<[T]> {}
+
+impl<T> Heap<T> for &mut [T] {}
+
+impl<T, const N: usize> Heap<T> for [T; N] {}
+
+#[cfg(feature = "alloc")]
+impl<T> Heap<T> for alloc::vec::Vec<T> {}
--- vm/rust/src/lib.rs
+++ vm/rust/src/lib.rs
@@ -2,8 +2,13 @@
 
 #![no_std]
 
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+mod heap;
 mod integer;
 mod value;
 
+pub use heap::Heap;
 pub use integer::Integer;
 pub use value::{Value, Value32, Value64};
