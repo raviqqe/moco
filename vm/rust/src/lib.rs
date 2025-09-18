8b52c3703262b8e210a906b681bdeaecfc2e733e
Yota Toyama <raviqqe@gmail.com>
2025-09-17 20:48:49 -0700

    Fix

--- vm/rust/src/lib.rs
+++ vm/rust/src/lib.rs
@@ -7,8 +7,10 @@ extern crate alloc;
 
 mod heap;
 mod integer;
+mod memory;
 mod value;
 
 pub use heap::Heap;
 pub use integer::Integer;
+pub use memory::Memory;
 pub use value::{Value, Value32, Value64};
