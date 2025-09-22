96f4452c2a192a39eaa80f498793b2229f6d0670
Yota Toyama <raviqqe@gmail.com>
2025-09-21 17:59:05 -0700

    Refactor

--- vm/rust/src/cons.rs
+++ vm/rust/src/cons.rs
@@ -9,21 +9,25 @@ pub struct Cons<V>(V);
 
 impl<V: Value> Cons<V> {
     /// Creates a cons.
+    #[inline]
     pub fn new(index: usize) -> Self {
         Self(V::from_pointer(V::Pointer::from_usize(index) << Tag::BITS))
     }
 
     /// Returns an index.
+    #[inline]
     pub fn index(self) -> usize {
         (self.0.to_pointer() >> Tag::BITS).to_usize()
     }
 
     /// Returns a tag.
+    #[inline]
     pub fn tag(self) -> Tag {
         self.0.to_pointer().to_usize() as _
     }
 
     /// Sets an index.
+    #[inline]
     pub fn set_index(self, index: usize) -> Self {
         Self(self.0.set_pointer(
             self.0.to_pointer() & Tag::MAX.into() | (V::Pointer::from_usize(index) << Tag::BITS),
@@ -31,6 +35,7 @@ impl<V: Value> Cons<V> {
     }
 
     /// Sets a tag.
+    #[inline]
     pub fn set_tag(self, tag: Tag) -> Self {
         Self(
             self.0
@@ -39,12 +44,14 @@ impl<V: Value> Cons<V> {
     }
 
     /// Converts a cons to a value.
+    #[inline]
     pub const fn to_value(self) -> V {
         self.0
     }
 }
 
 impl<V: Value> From<V> for Cons<V> {
+    #[inline]
     fn from(value: V) -> Self {
         debug_assert!(value.is_pointer());
 
