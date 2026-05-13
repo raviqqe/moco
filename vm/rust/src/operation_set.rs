9477e51cec954b3d5344a3ceffb1dc9bc5c4d188
Yota Toyama <raviqqe@gmail.com>
2026-05-12 22:06:46 -0700

    fix

--- vm/rust/src/operation_set.rs
+++ vm/rust/src/operation_set.rs
@@ -1,5 +1,9 @@
 /// An operation set.
 pub trait OperationSet {
-    fn add(&mut self, operation: String);
-    fn contains(&self, operation: &str) -> bool;
+    /// An error.
+    type Error: Exception;
+
+    /// Runs a primitive on a virtual machine.
+    #[maybe_async]
+    fn operate(&mut self, memory: &mut Memory<H>, primitive: usize) -> Result<(), Self::Error>;
 }
