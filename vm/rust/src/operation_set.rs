d076f5e8ab944ce51e7c963c9049173695d82482
Yota Toyama <raviqqe@gmail.com>
2026-05-12 22:07:46 -0700

    fix

--- vm/rust/src/operation_set.rs
+++ vm/rust/src/operation_set.rs
@@ -5,6 +5,6 @@ pub trait OperationSet<V, H> {
     /// An error.
     type Error;
 
-    /// Runs a primitive on a virtual machine.
+    /// Runs an operation.
     fn operate(&mut self, memory: &mut Memory<V, H>, primitive: usize) -> Result<(), Self::Error>;
 }
